eed8865cdd4f1e41dfd855ee644676ef82b2da91
Elias <elias@baez.us>
2021-01-14 20:32:09 -0500

    Update to say hi to alex.

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const http = require('http');
 
 http.createServer(function(request, response) {
   response.writeHead(200, { "Content-Type": "text/plain"});
-  response.write('hello baezus');
+  response.write('hello alex');
   response.end();
 }).listen(80);
